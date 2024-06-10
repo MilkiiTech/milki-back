56cbbbb99dfbdd7dfc444b1f86a8743340da96cc
Amir <amaedris1@gmail.com>
2024-06-10 16:15:33 +0300

    create sector validation

--- structure/validation/sector/sector.validation.js
+++ structure/validation/sector/sector.validation.js
@@ -1,36 +1,33 @@
 
 const Joi = require("joi");
 const createSectorValidation = (req, res, next) => {
-  // Define Joi schema for the form data
-  const phonePattern = /^(\+251|0)9\d{8}$/;
-  const schema = Joi.object({
-    userDetail: Joi.object({
-      username:Joi.string().required(),
-      email: Joi.string()
-      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-      phone_number: Joi.string().pattern(phonePattern),
-  }),
-  woredaDetail: Joi.object({
-      woreda_name:Joi.string().required(),
-      email_address: Joi.string()
-      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-      contact_phone_number: Joi.string().pattern(phonePattern),
-      city_name:Joi.string().required(),
-  }),
-  role_id:Joi.number().required(),
-  zone_id:Joi.string().guid({ version: ['uuidv4'] }).required()
+    // Define Joi schema for the form data
     
-  });
-
-  // Validate the request body
-  const { error } = schema.validate(req.body);
-  if (error) {
-    console.log(error, "error");
-    return res.status(400).json({ error: error.details[0].message });
-  }
-
-  next();
-};
+    const schema = Joi.object({
+      sector_name: Joi.string().required(),
+      sector_type: Joi.string().valid('Zone', 'Woreda').required(),
+      zone_id: Joi.number().integer().positive().when('sector_type', {
+        is: 'Zone',
+        then: Joi.required(),
+        otherwise: Joi.allow(null)
+      }),
+      woreda_id: Joi.number().integer().positive().when('sector_type', {
+        is: 'Woreda',
+        then: Joi.required(),
+        otherwise: Joi.allow(null)
+      })
+      
+    });
+  
+    // Validate the request body
+    const { error } = schema.validate(req.body);
+    if (error) {
+      console.log(error, "error");
+      return res.status(400).json({ error: error.details[0].message });
+    }
+  
+    next();
+  };
 
 
 module.exports = {
