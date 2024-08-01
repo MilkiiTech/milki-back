c5d397f5198b83b7ad50ff2b53f980bc5fd855c5
Amir <amaedris1@gmail.com>
2024-08-01 15:53:07 +0300

    work router modification

--- structure/routers/work/work.routes.js
+++ structure/routers/work/work.routes.js
@@ -180,11 +180,151 @@ router.delete("/:workId", checkPermission('can_delete_zone_admin'), deleteOne);
  */
 
 router.post("/assign/:workId", checkPermission("can_update_work"),assignWorkToSector);
-
-router.get("/getByUserId", checkPermission("can_view_work"),getWorkByUserId)
+/**
+ * @swagger
+ * /structure/work/getByUserId:
+ *   get:
+ *     summary: Fetch Work by UserId
+ *     tags: [Work]
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               work: {}
+ */
+router.get("/getByUserId", checkPermission("can_view_work"),getWorkByUserId);
+/**
+ * @swagger
+ * /structure/work/pickWork:
+ *   post:
+ *     summary: Fetch Work by workId
+ *     tags: [Work]
+ *     parameters:
+ *       - name: workId
+ *         in: path
+ *         required: true
+ *         description: The ID of the work
+ *         schema:
+ *           type: string
+ *         example:
+ *             a5166002-89bb-4a3c-a34a-39b44d33c212
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               work: {}
+ */
 router.post("/pickWork", checkPermission("can_update_work"),pickWork)
+/**
+ * @swagger
+ * /structure/work/weeklyTask/:
+ *   post:
+ *     summary: create New Weekly Task for Work
+ *     tags: [Work]
+ *     requestBody:
+ *       description: Request body for assigning work to sectors
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               sector_ids:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *               weekNumber:
+ *                 type: string
+ *             example:
+ *               sector_ids:
+ *                 - "5725a78a-98cf-4da8-96ab-cc72a5b5af0a"
+ *               weekNumber: "4"
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: "Work assigned successfully"
+ *       400:
+ *         description: Invalid request
+ */
+
 router.post("/weeklyTask/", checkPermission("can_create_weeklyTask"),createWeaklyTask);
+/**
+ * @swagger
+ * /structure/work/weeklyTask/{weekly_task_id}:
+ *   get:
+ *     summary: Fetch Work by workId
+ *     tags: [Work]
+ *     parameters:
+ *       - name: weekly_task_id
+ *         in: path
+ *         required: true
+ *         description: The ID of the Weekly Task
+ *         schema:
+ *           type: string
+ *         example:
+ *             a5166002-89bb-4a3c-a34a-39b44d33c212
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               work: {}
+ */
 router.get("/weeklyTask/:weekly_task_id", checkPermission("can_view_weeklyTask"),getWeeklyTaskById);
+/**
+ * @swagger
+ * /structure/work/weeklyTask/{workId}:
+ *   get:
+ *     summary: Fetch Work by workId
+ *     tags: [Work]
+ *     parameters:
+ *       - name: workId
+ *         in: path
+ *         required: true
+ *         description: The ID of the Weekly Task
+ *         schema:
+ *           type: string
+ *         example:
+ *             a5166002-89bb-4a3c-a34a-39b44d33c212
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               work: {}
+ */
 router.get("/weeklyTask/:workId", checkPermission("can_view_weeklyTask"),getWeeklyTask);
+/**
+ * @swagger
+ * /structure/work/weeklyTask/{weekly_task_id}:
+ *   put:
+ *     summary: Fetch Work by workId
+ *     tags: [Work]
+ *     parameters:
+ *       - name: weekly_task_id
+ *         in: path
+ *         required: true
+ *         description: The ID of the Weekly Task
+ *         schema:
+ *           type: string
+ *         example:
+ *             a5166002-89bb-4a3c-a34a-39b44d33c212
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               work: {}
+ */
 router.put("/weeklyTask/:weekly_task_id", checkPermission("can_update_weeklyTask"), updateWeeklyTask);
 module.exports = router;
